f3f641f87fdddb70ac74d5a2742c4071aca9ff3a
Vulnet Sejfulai <timewebsorder@gmail.com>
2024-02-29 11:59:43 +0100

    important

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  important: true,
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx,html}',
     './components/**/*.{js,ts,jsx,tsx,mdx,html}',
